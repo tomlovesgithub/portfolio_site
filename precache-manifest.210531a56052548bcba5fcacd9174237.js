275266a69b1a107cc71e08661a626f7c90850c2b
Tom Russell <TR115251@hotmail.co.uk>
2020-02-14 19:47:48 +0000

    Updates

---
+++ precache-manifest.210531a56052548bcba5fcacd9174237.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb3294d07992d043787ed7378761180c",
+    "url": "/portfolio_site/index.html"
+  },
+  {
+    "revision": "43544b1be82f2e85467d",
+    "url": "/portfolio_site/static/css/main.b3dfa2b5.chunk.css"
+  },
+  {
+    "revision": "aba345c069834f583f19",
+    "url": "/portfolio_site/static/js/2.8003f11e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portfolio_site/static/js/2.8003f11e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43544b1be82f2e85467d",
+    "url": "/portfolio_site/static/js/main.cffb6cdc.chunk.js"
+  },
+  {
+    "revision": "df2ed42c26eee23c001e",
+    "url": "/portfolio_site/static/js/runtime-main.20783efa.js"
+  }
+]);
